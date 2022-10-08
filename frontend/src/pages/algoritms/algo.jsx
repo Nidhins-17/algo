0d5a8a316611a47bf980a85a360f51b656b26225
prat <pratikkatti4@gmial.com>
2022-10-08 18:43:31 +0530

    routing phase1

---
+++ frontend/src/pages/algoritms/algo.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function algo() {
+  return (
+    <div>algo</div>
+  )
+}
--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div>
        <h1 className="text">ALGORITHM VISUALIZER</h1>
-        <h4 className="text1">Visualize Algoritm For Better Understanding</h4>
+        <h3 className="text1">Visualize Algoritm For Better Understanding</h3>
 
         <Card/>
     </div>
